06d4f784fdf27cd1841cbb61be8cf45f63e07637
Chris Aprea <chris.aprea@gmail.com>
2014-04-09 15:31:11 +1000

    Cleaning up the plugin headers #10

--- advanced-excerpt.php
+++ advanced-excerpt.php
@@ -1,26 +1,11 @@
 <?php
 /*
 Plugin Name: Advanced Excerpt
-Plugin URI: http://basvd.com/code/advanced-excerpt/
-Description: Several improvements over WP's default excerpt. The size of the excerpt can be limited using character or word count, and HTML markup is not removed.
+Plugin URI: http://deliciousbrains.com/advanced-excerpt/
+Description: Control the appearance of WordPress post excerpts
 Version: 4.1.1
-Author: Bas van Doren
-Author URI: http://basvd.com/
-
-Copyright 2007 Bas van Doren
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Author: Delicious Brains
+Author URI: http://deliciousbrains.com/
 */
 
 $GLOBALS['advanced_excerpt_version'] = '4.1.1';
