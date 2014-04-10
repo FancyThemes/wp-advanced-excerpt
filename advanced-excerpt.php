e5e65e2b9ed34af6768cd865ec5a9f2415c3bd08
Chris Aprea <chris.aprea@gmail.com>
2014-04-10 10:08:49 +1000

    Removing redundant PHP version check #10

--- advanced-excerpt.php
+++ advanced-excerpt.php
@@ -12,15 +12,6 @@ $GLOBALS['advanced_excerpt_version'] = '4.1.1';
 
 load_plugin_textdomain( 'advanced-excerpt', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-if ( version_compare( PHP_VERSION, '5.2', '<' ) ) {
-	// Thanks for this Yoast!
-	if ( is_admin() && ( !defined( 'DOING_AJAX' ) || !DOING_AJAX ) ) {
-		require_once ABSPATH.'/wp-admin/includes/plugin.php';
-		deactivate_plugins( __FILE__ );
-		wp_die( __( 'Advanced Excerpt requires PHP 5.2 or higher, as does WordPress 3.2 and higher. The plugin has now disabled itself.', 'advanced-excerpt' ) );
-	}
-}
-
 require_once 'class/advanced-excerpt.php';
 require_once 'functions/functions.php';
 
